7fcb575ed4589e841010bf121689cf22f31c3810
sapreet-autviz <sapreet.singh@autviz.in>
2025-01-02 16:53:29 +0530

    config database or set connectionString.

---
+++ All-Social-media-apps-apis/Migrations/20250102112025_intiload.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace All_Social_media_apps_apis.Migrations
+{
+    /// <inheritdoc />
+    public partial class intiload : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- All-Social-media-apps-apis/Program.cs
+++ All-Social-media-apps-apis/Program.cs
@@ -1,11 +1,21 @@
+using All_Social_media_apps_apis.DataBase;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Set DataBase Connection
+var connectionString = builder.Configuration.GetConnectionString("dbString");
+builder.Services.AddDbContext<Applicationdbcontext>(options =>
+    options.UseSqlServer(connectionString)
+);
 
 var app = builder.Build();
 
