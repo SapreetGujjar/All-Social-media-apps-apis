cb12dc01a2d86d9f2f8b557c4070d16cd7c87b9d
sapreet-autviz <sapreet.singh@autviz.in>
2025-01-02 16:43:45 +0530

    Add project files.

---
+++ All-Social-media-apps-apis/DataBase/Applicationdbcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace All_Social_media_apps_apis.DataBase
+{
+    public class Applicationdbcontext:DbContext
+    {
+        public Applicationdbcontext(DbContextOptions<Applicationdbcontext> options):base(options)
+        {
+            
+        }
+    }
+}
---
+++ All-Social-media-apps-apis/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace All_Social_media_apps_apis
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
